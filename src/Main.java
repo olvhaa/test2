3eaab9d4b36c009029a3edd88e97e87c3503890e
Olga <olga.przybys@onet.pl>
2023-09-07 15:32:50 +0200

    uwu

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        int skladnik1 = 15;
+        System.out.println(skladnik1);
+        int skladnik2 = 9;
+        System.out.println(skladnik2);
+        int suma = skladnik1 + skladnik2;
+        System.out.println(suma);
+
+        Scanner czytnik = new Scanner(System.in);
+        System.out.println("Podaj liczbę naturalną");
+        int skladnik = czytnik.nextInt();
+       // skladnik = czytnik.nextInt();
+        System.out.println(skladnik + skladnik1);
+
     }
 }
